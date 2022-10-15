a09f22a79b8645443680d1403252b8d1653e7251
JairDavid <20193tn071@utez.edu.mx>
2022-10-14 23:37:57 -0500

    add manifest and mobile icons

--- sw.js
+++ sw.js
@@ -20,11 +20,19 @@ self.addEventListener('install',(event)=>{
             '/index.html',
             '/login.html',
             '/template.html',
-            '/img/Card 1.svg',
-            '/img/Card 2.svg',
-            '/img/Card 3.svg',
-            '/img/Logo.png',
             '/js/app.js',
+            '/sw.js',
+            '/manifest.json',
+            '/images/Card 1.svg',
+            '/images/Card 2.svg',
+            '/images/Card 3.svg',
+            '/images/Logo.png',
+            '/images/icons/android-launchericon-48-48.png',
+            '/images/icons/android-launchericon-72-72.png',
+            '/images/icons/android-launchericon-96-96.png',
+            '/images/icons/android-launchericon-144-144.png',
+            '/images/icons/android-launchericon-192-192.png',
+            '/images/icons/android-launchericon-512-512.png',
             'https://img.freepik.com/free-photo/colorful-confetti-sprinkles-textured-background_53876-31921.jpg?w=2000',
             'https://www.londonperfect.com/blog/wp-content/uploads/2018/09/7-Sweet-Shops-In-London-by-London-Perfect1.jpg'
         ])
@@ -32,11 +40,7 @@ self.addEventListener('install',(event)=>{
     event.waitUntil(Promise.all([inmutablePromise, staticPromise]))
 })
 
+//Only cache
 self.addEventListener('fetch', (event)=>{
-
-    if(event.request.url.includes("https://")){
-        event.respondWith(fetch(event.request))
-    }else{
-        event.respondWith(caches.match(event.request))
-    }
+    event.respondWith(caches.match(event.request))
 })
