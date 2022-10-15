337ea0c27f358efb04a2189d03f39eda566251a7
JairDavid <20193tn071@utez.edu.mx>
2022-10-15 14:00:27 -0500

    add tailwind lib + configs

--- sw.js
+++ sw.js
@@ -9,7 +9,8 @@ self.addEventListener('install',(event)=>{
             'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap',
             'https://fonts.googleapis.com/icon?family=Material+Icons',
             'https://cdn.jsdelivr.net/npm/daisyui@2.31.0/dist/full.css',
-            'https://cdn.tailwindcss.com'
+            'https://fonts.gstatic.com/s/materialicons/v139/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2',
+            ''
         ])
     });
 
@@ -22,19 +23,21 @@ self.addEventListener('install',(event)=>{
             '/template.html',
             '/js/app.js',
             '/sw.js',
+            '/tailwind.config.js',
+            '/dist/output.css',
             '/manifest.json',
             '/images/Card 1.svg',
             '/images/Card 2.svg',
             '/images/Card 3.svg',
             '/images/Logo.png',
+            '/images/london.jpg',
+            '/images/candies.png',
             '/images/icons/android-launchericon-48-48.png',
             '/images/icons/android-launchericon-72-72.png',
             '/images/icons/android-launchericon-96-96.png',
             '/images/icons/android-launchericon-144-144.png',
             '/images/icons/android-launchericon-192-192.png',
             '/images/icons/android-launchericon-512-512.png',
-            'https://img.freepik.com/free-photo/colorful-confetti-sprinkles-textured-background_53876-31921.jpg?w=2000',
-            'https://www.londonperfect.com/blog/wp-content/uploads/2018/09/7-Sweet-Shops-In-London-by-London-Perfect1.jpg'
         ])
     }); 
     event.waitUntil(Promise.all([inmutablePromise, staticPromise]))
