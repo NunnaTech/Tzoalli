a15dc11482f224eff919705e87a399518cba67e7
JairDavid <20193tn071@utez.edu.mx>
2022-11-14 13:25:40 -0600

    add views consuming rest api

--- js/services/VisitService.js
+++ js/services/VisitService.js
@@ -13,4 +13,28 @@ export class VisitService {
             })
     }
 
+    GetAllPendingVisits() {
+        return fetch("http://127.0.0.1:8000/api/visit/getMyVisits/En camino",
+            {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': "Bearer 2|yxcPDjc23t9wWR2Cf5GEEQxBHMZTHcbVnquQJzG0"
+                }
+            })
+    }
+
+    GetAllNextVisits() {
+        return fetch("http://127.0.0.1:8000/api/visit/getMyVisits/Pendiente",
+            {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': "Bearer 2|yxcPDjc23t9wWR2Cf5GEEQxBHMZTHcbVnquQJzG0"
+                }
+            })
+    }
+
 }
